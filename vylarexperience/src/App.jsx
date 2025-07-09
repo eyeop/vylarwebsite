4885ea62d1d184fab2974eb9f4a1b3f77eed8fbf
Jaewon Lee <68724307+eyeop@users.noreply.github.com>
2025-07-09 23:57:44 +0900

    Update App.jsx
    Add a bg-black class to the background image div so any area not covered by the image will be black.

--- vylarexperience/src/App.jsx
+++ vylarexperience/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       <div
-        className="bg-cover bg-center bg-no-repeat fixed inset-0 -z-10"
+        className="bg-black bg-cover bg-center bg-no-repeat fixed inset-0 -z-10"
         style={backgroundStyle}
       />
       <div className="relative min-h-screen max-h-screen overflow-auto">
